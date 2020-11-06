6245fa9fff60be6798cf6a23f899185515d7456f
vedanttiwari42 <tiwari.vedant42@gmail.com>
2020-11-07 00:58:38 +0530

    Initial Commit

--- LinkedList/src/DoublyAndCircularLinkedList/InsertionOperations.java
+++ LinkedList/src/DoublyAndCircularLinkedList/InsertionOperations.java
@@ -12,4 +12,25 @@ public class InsertionOperations {
         }
         return head;
     }
+
+    static Node insertNodeAtKthPosition(Node head,int k,int position){
+        Node newNode = new Node(k);
+        if(position == 1){
+            newNode.next=head;
+            head.prev=newNode;
+            head = newNode;
+        } else{
+            Node currentNode = head, temp = head;
+            int i = 1;
+            while(i < position){
+                currentNode = temp;
+                temp = temp.next;
+            }
+            newNode.next = curr.next;
+            temp.prev = newNode;
+            currentNode.next = newNode;
+            newNode.prev = currentNode;
+        }
+        return head;
+    }
 }
